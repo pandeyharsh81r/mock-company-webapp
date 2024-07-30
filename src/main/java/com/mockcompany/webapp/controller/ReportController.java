9ca3f90d793543546fff1895eccb330d380e1d16
pandeyharsh81r <144554829+pandeyharsh81r@users.noreply.github.com>
2024-07-31 00:48:22 +0530

    Update ReportController.java

--- src/main/java/com/mockcompany/webapp/controller/ReportController.java
+++ src/main/java/com/mockcompany/webapp/controller/ReportController.java
@@ -10,6 +10,7 @@ import javax.persistence.EntityManager;
 import java.util.HashMap;
 import java.util.Map;
 
+
 /**
  * Management decided it is super important that we have lots of products that match the following terms.
  * So much so, that they would like a daily report of the number of products for each term along with the total
@@ -17,41 +18,43 @@ import java.util.Map;
  */
 @RestController
 public class ReportController {
-
-    // After reading code/tests, we can capture the important terms in an array!
-    private static final String[] importantTerms = new String[] {
+    // Capturing important terms as mentioned in code/tests in an array
+    private static final String[] importantTerms = new String[]{
             "Cool",
             "Amazing",
             "Perfect",
             "Kids"
     };
-
     private final EntityManager entityManager;
-    // Declare SearchService same as EntityManager
+    // Declare SearchService as instance field of the below constructor same as EntityManager
     private final SearchService searchService;
 
-    // Add the SearchService to the constructor
+    // Pass SearchService as the parameter of the constructor
     @Autowired
     public ReportController(EntityManager entityManager, SearchService searchService) {
         this.entityManager = entityManager;
         this.searchService = searchService;
     }
 
+
     @GetMapping("/api/products/report")
     public SearchReportResponse runReport() {
-        // We could use the search service and do an empty string query to get the count but this is much more efficient
+
+        // Query Creation for directly getting the count of items
         Number count = (Number) this.entityManager.createQuery("SELECT count(item) FROM ProductItem item").getSingleResult();
 
-        // For each important term, query on it and add size of results to our Map
+        // Executing query on each important terms and then add the size of results to the Map
         Map<String, Integer> hits = new HashMap<>();
-        for (String term : importantTerms) {
+        for(String term : importantTerms) {
             hits.put(term, searchService.search(term).size());
         }
 
-        // Transform to API response and return
+        // Generating API response and returning it
         SearchReportResponse response = new SearchReportResponse();
         response.setProductCount(count.intValue());
         response.setSearchTermHits(hits);
+
+
         return response;
     }
 }
