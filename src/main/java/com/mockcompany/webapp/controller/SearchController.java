c3e325804bd8764a80d5def638d153fbb70dfbf1
pandeyharsh81r <144554829+pandeyharsh81r@users.noreply.github.com>
2024-07-31 00:48:56 +0530

    Update SearchController.java

--- src/main/java/com/mockcompany/webapp/controller/SearchController.java
+++ src/main/java/com/mockcompany/webapp/controller/SearchController.java
@@ -1,22 +1,11 @@
-/*
- * Java classes are grouped in "packages". This allows them to be referenced and used in other
- * classes using import statements.  Any class in this project is prefixed in the com.mockcompany.webapp
- * package.
- *
- *   https://www.w3schools.com/java/java_packages.asp
- *
- * For general help with Java, see the tutorialspoint tutorial:
- *
- *   https://www.tutorialspoint.com/java/index.htm
- */
 package com.mockcompany.webapp.controller;
 
 /*
  * An import statement allows the current class to use the class being imported
  */
 import com.mockcompany.webapp.model.ProductItem;
-import com.mockcompany.webapp.service.SearchService;
 /* The springframework package allows us to take advantage of the spring capabilities */
+import com.mockcompany.webapp.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /* java.util package provides useful utilities */
 import java.util.Collection;
+
 
 /**
  * This class is the entrypoint for the /api/products/search API.  It is "annotated" with
@@ -40,10 +30,10 @@ import java.util.Collection;
 @RestController
 public class SearchController {
 
-    // Inject the new service into our class
+    // Implementing SearchService in SearchController class
     private final SearchService searchService;
 
-    // Update constructor to inject SearchService
+    // Updating constructor for implementing SearchService
     @Autowired
     public SearchController(SearchService searchService) {
         this.searchService = searchService;
@@ -54,12 +44,15 @@ public class SearchController {
      * when an HTTP GET on the path /api/products/search is made.  A single query parameter is declared
      * using the @RequestParam annotation.  The value that is passed when performing a query will be
      * in the query parameter.
+     *
      * @param query
      * @return The filtered products
      */
     @GetMapping("/api/products/search")
     public Collection<ProductItem> search(@RequestParam("query") String query) {
-        // Implementation of method was moved to service, delegate query to the service now
+        // Method implementation was moved to SearchService, delegate query to the service
         return this.searchService.search(query);
+
     }
 }
+
